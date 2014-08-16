96021ef66c041aef08be55ec929effdd4e35c125
Matt S Trout <mst@shadowcat.co.uk>
2014-08-16 18:42:18 +0000

    minor doc improvements

--- lib/Object/Tap.pm
+++ lib/Object/Tap.pm
@@ -44,6 +44,11 @@ or use a method name instead of a sub ref -
 
   my $thing = My::Class->new(...)->$_tap(set_foo => 1);
 
+For a 'real' example of how that might be used, one could create and
+initialize an L<HTML::TableExtract> object in one go using -
+
+  my $te = HTML::TableExtract->new->$_tap(parse => $html);
+
 =head1 AUTHOR
 
 mst - Matt S. Trout (cpan:MSTROUT) <mst@shadowcat.co.uk>
